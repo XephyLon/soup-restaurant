c7d12e12b0a2c13551c55247b1c792c4ff800d53
Xephy <adr3nalinexin@gmail.com>
2018-10-04 16:55:51 +0200

    added basic work for main section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { tns } from 'tiny-slider/src/tiny-slider';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
+
+  constructor() {
+    tns();
+  }
+
   title = 'soup-restaurant';
 }
