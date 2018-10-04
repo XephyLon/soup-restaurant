ee547131dbdb3d2bf7645fb511bbbcb7eb184d50
Xephy <adr3nalinexin@gmail.com>
2018-10-04 15:41:50 +0200

    finished front-end part for header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   logoPath = 'assets/logo.svg';
+  arrowPath = 'assets/arrow-down.svg';
+  cartPath = 'assets/shopping-cart.svg';
+  list = [
+    {
+      class: 'menu-item has-dropdown', name: 'home', hasDropdown: true, dropdownList: [
+        'home basic',
+        'home fullwidth slider',
+        'home home video'
+      ]},
+    { class: 'menu-item has-dropdown', name: 'about', hasDropdown: true, dropdownList: [
+      'about us',
+      'services',
+      'gallery',
+      'reviews',
+      'faq'
+    ]},
+    { class: 'menu-item has-dropdown', name: 'menu', hasDropdown: true, dropdownList: [
+      'list',
+      'grid'
+    ]},
+    { class: 'menu-item', name: 'offers', hasDropdown: false},
+    { class: 'menu-item', name: 'contact', hasDropdown: false},
+    { class: 'menu-item has-dropdown', name: 'more', hasDropdown: true, dropdownList: [
+      'book a table',
+      'blog',
+      'blog + sidebar',
+      'blog post'
+    ]}
+  ];
 }
