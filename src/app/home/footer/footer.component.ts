1b0117c6415d6641dd1d422af211e8e198fafdbe
Xephy <adr3nalinexin@gmail.com>
2018-10-09 13:16:16 +0200

    finished styling footer and promo sections

--- src/app/home/footer/footer.component.ts
+++ src/app/home/footer/footer.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { faFacebook, faGoogle, faTwitter, faYoutube, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-footer',
@@ -7,6 +8,9 @@ import { faFacebook, faGoogle, faTwitter, faYoutube, faInstagram } from '@fortaw
   styleUrls: ['./footer.component.sass']
 })
 export class FooterComponent {
+
+  faArrowRight = faArrowRight
+
   newsList = [
     {title: 'How to make perfect pasta?', date: 'May 21, 2017'},
     {title: 'How to make perfect pasta?', date: 'May 21, 2017'},
@@ -14,10 +18,10 @@ export class FooterComponent {
   ]
 
   socialMedia = [
-    {class: 'footer__icon--facebook', url: 'http://fb.com', icon: faFacebook},
-    {class: 'footer__icon--google', url: 'http://gp.com', icon: faGoogle},
-    {class: 'footer__icon--twitter', url: 'http://tw.com', icon: faTwitter},
-    {class: 'footer__icon--youtube', url: 'http://yt.com', icon: faYoutube},
-    {class: 'footer__icon--instagram', url: 'http://ins.com', icon: faInstagram}
+    {class: 'footer__icon__social__facebook', url: 'http://fb.com', icon: faFacebook},
+    {class: 'footer__icon__social__google', url: 'http://gp.com', icon: faGoogle},
+    {class: 'footer__icon__social__twitter', url: 'http://tw.com', icon: faTwitter},
+    {class: 'footer__icon__social__youtube', url: 'http://yt.com', icon: faYoutube},
+    {class: 'footer__icon__social__instagram', url: 'http://ins.com', icon: faInstagram}
   ]
 }
