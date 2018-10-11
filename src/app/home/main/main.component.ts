f950d34e442eb718cf8ec8d36f061632446a83bb
Xephy <adr3nalinexin@gmail.com>
2018-10-11 15:14:50 +0200

    Implemented a carousel

--- src/app/home/main/main.component.ts
+++ src/app/home/main/main.component.ts
@@ -1,16 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef, Input, ChangeDetectionStrategy } from '@angular/core';
+import { NguCarouselConfig } from '@ngu/carousel';
+import { interval, Observable } from 'rxjs';
+import { startWith, take, map } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.sass']
+  styleUrls: ['./main.component.sass'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+
 })
 export class MainComponent {
+  @Input() name: string;
+  imgags = [
+    'assets/images/slider-burger-1.jpg',
+    'assets/images/slider-pasta-1.jpg',
+    'assets/images/slider-dessert-1.jpg',
+    'assets/holi.jpg'
+  ];
+  public carouselTileItems$: Observable<number[]>;
+  public carouselTileConfig: NguCarouselConfig = {
+    grid: { xs: 1, sm: 1, md: 1, lg: 1, all: 0 },
+    speed: 250,
+    point: {
+      visible: true
+    },
+    touch: true,
+    loop: true,
+    interval: { timing: 2000 },
+    animation: 'lazy'
+  };
+  tempData: any[];
 
-  public carouselItems = [
-    {imagePath: 'assets/images/slider-burger-1.jpg', alt: 'Burger'},
-    {imagePath: 'assets/images/slider-pasta-1.jpg', alt: 'Pasta'},
-    {imagePath: 'assets/images/slider-dessert-1.jpg', alt: 'Dessert'}
-  ]
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngOnInit() {
+    this.tempData = [];
+    this.carouselTileItems$ = interval(500).pipe(
+      startWith(-1),
+      take(30),
+      map(() => {
+        const data = (this.tempData = [
+          ...this.tempData,
+          this.imgags[Math.floor(Math.random() * this.imgags.length)]
+        ]);
+        return data;
+      })
+    );
+  }
 
 }
