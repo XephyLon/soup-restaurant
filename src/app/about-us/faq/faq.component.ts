ad16393e83ff7bc897d1038b90963f7602fa9776
Xephy <adr3nalinexin@gmail.com>
2018-10-14 12:18:26 +0200

    added styling for FAQ page

--- src/app/about-us/faq/faq.component.ts
+++ src/app/about-us/faq/faq.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { faFile } from '@fortawesome/free-regular-svg-icons';
+import { faCube, faWallet, faAlignJustify } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-faq',
   templateUrl: './faq.component.html',
   styleUrls: ['./faq.component.sass']
 })
-export class FaqComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class FaqComponent {
+  faFile = faFile
+  faAlignJustify = faAlignJustify
+  faqs = [
+    {title: 'General', icon: faFile},
+    {title: 'Delivery', icon: faCube},
+    {title: 'Payments', icon: faWallet}
+  ]
 
 }
