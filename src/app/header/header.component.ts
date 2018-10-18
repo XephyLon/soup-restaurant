34e719e0067c53294d87c7023caa50b323d6d554
Xephy <adr3nalinexin@gmail.com>
2018-10-18 11:53:09 +0200

    Added custom breakpoints and basic responsive styling to Header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faCartArrowDown } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.sass']
 })
 export class HeaderComponent {
+  faCartArrowDown = faCartArrowDown
   logoPath = 'assets/logo.svg';
   arrowPath = 'assets/arrow-down.svg';
   cartPath = 'assets/shopping-cart.svg';
