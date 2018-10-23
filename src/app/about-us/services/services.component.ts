8fbdefaa09c61e7692386fd9ca3ff8075a4f6a74
Xephy <adr3nalinexin@gmail.com>
2018-10-23 15:18:23 +0200

    Css being CSS again

--- src/app/about-us/services/services.component.ts
+++ src/app/about-us/services/services.component.ts
@@ -6,7 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./services.component.sass']
 })
 export class ServicesComponent {
-  private description: string = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  // tslint:disable-next-line:max-line-length
+  private description = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+
+  serviceList = [
+    { title: 'Birthdays', text: this.description, image: { 'background-image': "url('assets/images/service-birthday.jpg')" } },
+    { title: 'Engagements', text: this.description, image: { 'background-image': "url('assets/images/service-engagement.jpg')" } },
+    { title: 'Weddings', text: this.description, image: { 'background-image': "url('assets/images/service-weddings.jpg')" } }
+  ]
 
   isEven(index) {
     if (index % 2 === 0 || index === 0) {
@@ -16,9 +23,4 @@ export class ServicesComponent {
     }
   }
 
-  serviceList = [
-    { title: 'Birthdays', text: this.description, image: { 'background-image': "url('../../../assets/images/service-birthday.jpg')" } },
-    { title: 'Engagements', text: this.description, image: { 'background-image': "url('../../../assets/images/service-engagement.jpg')" } },
-    { title: 'Weddings', text: this.description, image: { 'background-image': "url('../../../assets/images/service-weddings.jpg')" } }
-  ]
 }
