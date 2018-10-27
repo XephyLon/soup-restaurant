71e6872991418ed325519eb94cd1b367b5293ee1
Xephy <adr3nalinexin@gmail.com>
2018-10-27 22:34:18 +0200

    More minor edits...can't wait to finish

--- src/app/carousels/menu-carousel/menu-carousel.component.ts
+++ src/app/carousels/menu-carousel/menu-carousel.component.ts
@@ -10,47 +10,43 @@ import { faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./menu-carousel.component.sass']
 })
 export class MenuCarouselComponent implements OnInit {
-
-  @Input() name: string
-  faArrowRight = faArrowRight
-  faArrowLeft = faArrowLeft
+  @Input()
+  name: string;
+  faArrowRight = faArrowRight;
+  faArrowLeft = faArrowLeft;
   images = [
-    'assets/images/menu-sample-burgers-1.jpg',
-    'assets/images/menu-title-desserts-1.jpg',
-    'assets/images/menu-title-drinks-1.jpg',
-    'assets/images/menu-sample-pasta-1.jpg',
-    'assets/images/menu-sample-pizza-1.jpg',
-    'assets/images/menu-sample-sushi-1.jpg'
-  ]
+    { title: 'Burgers', image: 'assets/images/menu-sample-burgers-1.jpg' },
+    { title: 'Desserts', image: 'assets/images/menu-title-desserts-1.jpg' },
+    { title: 'Drinks', image: 'assets/images/menu-title-drinks-1.jpg' },
+    { title: 'Pasta', image: 'assets/images/menu-sample-pasta-1.jpg' },
+    { title: 'Pizza', image: 'assets/images/menu-sample-pizza-1.jpg' },
+    { title: 'Sushi', image: 'assets/images/menu-sample-sushi-1.jpg' }
+  ];
 
-  public carouselTileItems$: Observable<number[]>
+  public carouselTileItems$: Observable<number[]>;
   public carouselTileConfig: NguCarouselConfig = {
     grid: { xs: 1, sm: 2, md: 2, lg: 3, all: 0 },
     speed: 550,
     point: { visible: true },
     touch: true,
     loop: true,
-    interval: { timing: 2000 },
+    interval: { timing: 5000 },
     animation: 'lazy'
-  }
+  };
 
-  tempData: any[]
+  tempData: any[];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-    this.tempData = []
+    this.tempData = [];
     this.carouselTileItems$ = interval(2000).pipe(
       startWith(this.images.length - 1),
       take(6),
-      map((val) => {
-        const data = (this.tempData = [
-          ...this.tempData,
-          this.images[val]
-        ])
-        return data
+      map(val => {
+        const data = (this.tempData = [...this.tempData, this.images[val]]);
+        return data;
       })
-    )
+    );
   }
-
 }
