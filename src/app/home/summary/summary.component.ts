08f17b3128227d278e00792d7ab1b6a4766b87d2
Xephy <adr3nalinexin@gmail.com>
2018-11-27 10:49:23 +0200

    No more stray blockquotes

--- src/app/home/summary/summary.component.ts
+++ src/app/home/summary/summary.component.ts
@@ -1,18 +1,28 @@
-import { Component } from '@angular/core';
-import { faStar, faShoppingCart, faWallet, faBox } from '@fortawesome/free-solid-svg-icons'
+import { BlockQuote } from './../../shared/blockquote.model';
+import { BlockquotesService } from './../../shared/blockquotes.service';
+import { Component, OnInit } from '@angular/core';
+import { faBox, faShoppingCart, faWallet } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-summary',
   templateUrl: './summary.component.html',
   styleUrls: ['./summary.component.sass']
 })
-export class SummaryComponent {
+export class SummaryComponent implements OnInit {
   bgPath = 'assets/images/bg-food-1.jpg'
-  faStar = faStar
+  quotes: Array<BlockQuote> = []
 
   optionsList = [
     {title: 'Pick a dish', text: 'Vivamus volutpat leo dictum risus ullamcorper condimentum.', icon: faShoppingCart},
     {title: 'Make a payment', text: 'Vivamus volutpat leo dictum risus ullamcorper condimentum.', icon: faWallet},
     {title: 'Recieve your food!', text: 'Vivamus volutpat leo dictum risus ullamcorper condimentum.', icon: faBox}
   ]
+
+  constructor(private blockquoteService: BlockquotesService) {}
+
+  ngOnInit(): void {
+    this.blockquoteService
+      .getQuotes()
+      .then(quotes => this.quotes = quotes)
+  }
 }
